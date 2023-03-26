1291974d91077adf9bfb6590a444a27d7117ae64
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-03-26 10:36:21 -0300

    Adiciona metódo para tratar exceções dos inputs do usuário

--- src/criadorsenhas/Main.java
+++ src/criadorsenhas/Main.java
@@ -1,16 +1,32 @@
 package criadorsenhas;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Qual o tamanho da sua senha?");
-        final int tamanhoSenha = scanner.nextInt();
-        System.out.println("Quantas senhas você quer criar?");
-        final int quantidadeSenha = scanner.nextInt();
+        String pergunta1 = "Qual o tamanho da sua senha?";
+        String pergunta2 = "Quantas senhas?";
+        int tamanhoSenha = validarEntrada(pergunta1);
+        int quantidadeSenha = validarEntrada(pergunta2);
 
         Senhas senha = new Senhas(tamanhoSenha, quantidadeSenha);
         senha.gerarSenha();
+    }
+
+    static int validarEntrada(String pergunta) {
+        boolean signal = true;
+        int input = 0;
+        do {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println(pergunta);
+                input = scanner.nextInt();
+                signal = false;
+            } catch (InputMismatchException ime) {
+                System.out.println("Digite um número inteiro!");
+            }
+        } while (signal);
+        return input;
     }
 }
