ef68577897444519761a4d1ce56899a7cb485d85
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-03-25 22:05:02 -0300

    Cria classe com construtor e implementa interface

---
+++ src/criadorsenhas/Senhas.java
@@ -0,0 +1,15 @@
+package criadorsenhas;
+
+public class Senhas implements Constantes{
+    private final int tamanhoSenha, quantidadeSenha;
+
+    protected Senhas(int tamanhoSenha, int quantidadeSenha) {
+        this.tamanhoSenha = tamanhoSenha;
+        this.quantidadeSenha = quantidadeSenha;
+    }
+
+    @Override
+    public void gerarSenha() {
+        
+    }
+}
