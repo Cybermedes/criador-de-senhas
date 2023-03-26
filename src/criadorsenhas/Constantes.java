7e2a98d76fa2f2b375d18cd59503f1ff943d423c
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-03-25 21:57:04 -0300

    Cria interface com constantes e metodo principal

---
+++ src/criadorsenhas/Constantes.java
@@ -0,0 +1,9 @@
+package criadorsenhas;
+
+public interface Constantes {
+    String letrasMinusculas = "abcdefghijklmnopqrstuvwxyz";
+    String letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String numeros = "0123456789";
+    String caracteres = "!?@#$&*(){}[]-=+\\/^~<>";
+    void gerarSenha();
+}
